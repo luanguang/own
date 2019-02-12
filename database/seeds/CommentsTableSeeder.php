6a3c31af8db1953aa5a7d1aab94ad63c8aa5f887
luanguang <523086505@qq.com>
2019-02-12 18:39:58 +0800

    config需要继续修改

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -12,19 +12,19 @@ class CommentsTableSeeder extends Seeder
     public function run()
     {
         DB::table('comments')->delete();
-        DB::table('comments')->insert([
-            [
-                'id'            => 1,
-                'oauth_user_id' => 1,
-                'type'          => 1,
-                'pid'           => 0,
-                'article_id'    => 1,
-                'content'       => '评论的内容',
-                'status'        => 1,
-                'created_at'    => '2017-7-16 07:35:12',
-                'updated_at'    => '2016-7-16 07:35:12',
-                'deleted_at'    => null,
-            ]
-        ]);
+        // DB::table('comments')->insert([
+        //     [
+        //         'id'            => 1,
+        //         'oauth_user_id' => 1,
+        //         'type'          => 1,
+        //         'pid'           => 0,
+        //         'article_id'    => 1,
+        //         'content'       => '评论的内容',
+        //         'status'        => 1,
+        //         'created_at'    => '2017-7-16 07:35:12',
+        //         'updated_at'    => '2016-7-16 07:35:12',
+        //         'deleted_at'    => null,
+        //     ]
+        // ]);
     }
 }
